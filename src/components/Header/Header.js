cb0ae515eb726c2ea2e8b8c5deac99fff6ba19c9
merczy04 <merczy04@yahoo.com>
2022-12-10 10:48:22 +0800

    initial

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React from "react";
 import Navbar from "../Navbar/Navbar";
 import SearchForm from "../SearchForm/SearchForm";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <div className='holder'>
-        <header className='header'>
-            <Navbar />
-            <div className='header-content flex flex-c text-center text-white'>
-                <h2 className='header-title text-capitalize'>find your book of choice.</h2><br />
-                <p className='header-text fs-18 fw-3'>Maybe this is why we read books, and why in moments of darkness we return to books: to find words for what we already know.</p>
-                <SearchForm />
-            </div>
-        </header>
+    <div className="holder">
+      <header className="header">
+        <Navbar />
+        <div className="header-content flex flex-c text-center text-white">
+          <h2 className="header-title text-capitalize">
+            find your book of choice.
+          </h2>
+          <br />
+          <p className="header-text fs-18 fw-3">
+            Maybe this is why we read books, and why in moments of darkness we
+            return to books: To find words for what we already know.
+          </p>
+          <SearchForm />
+        </div>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
